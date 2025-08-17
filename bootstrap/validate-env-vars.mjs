d7dd1fdfaca0b61e43345c5c069b831117c9ce2e
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2025-08-18 00:06:44 +0200

    chore(dev): validate env variables, load .env in dev environment
    Stop program if there is missing required environment variables

---
+++ bootstrap/validate-env-vars.mjs
@@ -0,0 +1,45 @@
+// Handle env vars here to avoid handling
+// missing env vars in the application code.
+// Let's just crash before starting if missing
+// values.
+const REQUIRED_ENV_VARS = {
+  DISCORD_TOKEN: {
+    sensitive: true,
+  },
+};
+
+// --- Functions ---
+
+function exitIfMissing(envVarName) {
+  const envVar = process.env[envVarName];
+  if (envVar === undefined || envVar.length === 0) {
+    console.error(`Missing required environment variable '${envVarName}'`);
+    process.exit(1);
+  }
+}
+
+function displayEnvironmentVariables() {
+  console.log(
+    "Starting the application with the following environment variables:"
+  );
+
+  Object.entries(REQUIRED_ENV_VARS).forEach(([envVarName, { sensitive }]) => {
+    const value = sensitive ? "<REDACTED>" : process.env[envVarName];
+    console.log(`- ${envVarName} = ${value}`);
+  });
+}
+
+// --- Code ---
+
+// Load environment variables from .env file in dev environment
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = await import("dotenv");
+  dotenv.config();
+}
+
+// Exit if any required environment variable is missing
+Object.keys(REQUIRED_ENV_VARS).forEach(exitIfMissing);
+
+// Display the environment variables being used
+displayEnvironmentVariables();
+console.log(); // separation with application logs
