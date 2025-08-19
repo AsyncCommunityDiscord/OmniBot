32c083305bdae3dfd2166392f8f640108c51ae10
RedsTom <tom.frefre.13@gmail.com>
2025-08-19 19:16:24 +0200

    feat: remove test modules (Prepare merge into master)

--- src/modules/example/listeners/message.listener.ts
+++
@@ -1,9 +0,0 @@
-import { declareEventListener } from "../../../lib/listener.js";
-import logger from "../../../lib/logger.js";
-
-export default declareEventListener({
-  eventType: "messageCreate",
-  async execute(message) {
-    logger.debug(`Message received: ${message.content}`);
-  },
-});
