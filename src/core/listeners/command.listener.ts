d3ef8387f302dca872e605a64bb3d38938d630f0
RedsTom <tom.frefre.13@gmail.com>
2025-08-19 15:37:35 +0200

    feat: enhance command handling to support module commands per guild

--- src/core/listeners/command.listener.ts
+++ src/core/listeners/command.listener.ts
@@ -2,21 +2,50 @@ import type {
   AutocompleteInteraction,
   ChatInputCommandInteraction,
 } from "discord.js";
+import { modules } from "../../index.js";
 import { declareEventListener } from "../../lib/listener.js";
 import logger from "../../lib/logger.js";
 import coreModule from "../core.module.js";
+import moduleService from "../services/module.service.js";
 
 async function handleCommand(interaction: ChatInputCommandInteraction) {
-  const command = coreModule.registry.commands.find(
-    (cmd) => cmd.data.name === interaction.commandName
-  );
+  const command = [...modules, coreModule]
+    .flatMap((module) =>
+      module.registry.commands.map((cmd) => ({
+        module: module,
+        command: cmd,
+      }))
+    )
+    .find((cmd) => cmd.command.data.name === interaction.commandName);
+
   if (!command) {
     logger.warn(`Command not found | name = ${interaction.commandName}`);
     return;
   }
+  logger.debug(
+    `Command found | name = ${command.command.data.name} | module = ${command.module.id}`
+  );
 
-  logger.debug(`Executing command | name = ${interaction.commandName}`);
-  await command.execute(interaction);
+  if (
+    command.module.id === coreModule.id ||
+    (
+      await moduleService.getModuleStateIn(
+        command.module.id,
+        interaction.guild!
+      )
+    ).activated
+  ) {
+    logger.debug(`Executing command | name = ${interaction.commandName}`);
+    await command.command.execute(interaction);
+  } else {
+    logger.warn(
+      `Command not enabled | name = ${interaction.command?.name} | module = ${command.module.id}`
+    );
+    await interaction.reply({
+      content: `The command \`${interaction.commandName}\` is not enabled in this guild.`,
+      ephemeral: true,
+    });
+  }
 }
 
 async function handleComplete(interaction: AutocompleteInteraction) {
