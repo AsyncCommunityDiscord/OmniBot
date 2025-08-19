3ba3b6368c0871c5489eb2c8b5ad1050837a8819
RedsTom <tom.frefre.13@gmail.com>
2025-08-19 16:16:54 +0200

    chore: improve logging capabilities

--- src/core/loaders/module-loader.ts
+++ src/core/loaders/module-loader.ts
@@ -2,8 +2,10 @@ import * as fs from "fs/promises";
 import { fileURLToPath, pathToFileURL } from "node:url";
 import path from "path";
 import { DeclarationType, type Declared } from "../../lib/declared.js";
-import logger from "../../lib/logger.js";
+import { loggerMaker } from "../../lib/logger.js";
 import type { Module } from "../../lib/module.js";
+
+const logger = loggerMaker("modulesr");
 
 const __dirname = path.resolve(
   fileURLToPath(import.meta.url),
@@ -13,7 +15,7 @@ const __dirname = path.resolve(
 );
 
 export async function loadModules(basePath: string): Promise<Module[]> {
-  logger.info("Loading modules from base path", basePath);
+  logger.info(`Finding modules | path = ${basePath}`);
 
   const modules: Module[] = [];
   const moduleFolder = path.resolve(__dirname, basePath);
@@ -41,20 +43,20 @@ export async function loadModules(basePath: string): Promise<Module[]> {
     }
   }
 
-  logger.info("Finished loading modules");
+  logger.info("Finished finding modules");
 
   return modules;
 }
 
 export async function loadModule(modulePath: string): Promise<Module | null> {
-  logger.info(`Loading module | path = ${modulePath}`);
+  logger.info(`\tResolving module | path = ${modulePath}`);
 
   const moduleEntryPoint = (await fs.readdir(modulePath)).find(
     (file) => file.match(/\.module\.ts$/) || file.match(/\.module\.js$/)
   );
 
   if (!moduleEntryPoint) {
-    logger.warn(`Module entry point not found | path = ${modulePath}`);
+    logger.warn(`\tModule entry point not found | path = ${modulePath}`);
     return null;
   }
 
@@ -65,17 +67,17 @@ export async function loadModule(modulePath: string): Promise<Module | null> {
   );
 
   if (!imported) {
-    logger.warn(`Failed to import module | path = ${moduleFilePath}`);
+    logger.warn(`\tFailed to import module | path = ${moduleFilePath}`);
     return null;
   }
 
   const module = imported.default;
   if (module.type !== DeclarationType.Module) {
-    logger.warn(`Invalid module | path = ${moduleFilePath}`);
+    logger.warn(`\tInvalid module | path = ${moduleFilePath}`);
     return null;
   }
 
-  logger.info(`Module loaded successfully | id = ${module.id}`);
+  logger.info(`\tModule resolved successfully | id = ${module.id}`);
 
   return module;
 }
