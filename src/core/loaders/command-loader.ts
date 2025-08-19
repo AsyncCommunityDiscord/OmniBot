bf96c38d9d83c3a5ecb5eb424e5fe7dceb52ed8e
RedsTom <tom.frefre.13@gmail.com>
2025-08-19 19:28:04 +0200

    fix: apply suggestion from review and update consolidated schema

--- src/core/loaders/command-loader.ts
+++ src/core/loaders/command-loader.ts
@@ -174,8 +174,7 @@ export async function checkCommandsForVersionChange(
       module.version
     );
     logger.info(
-      `\tSuccessfully updated commands in guild "${guild.id}" for module "${module.id}
-      `\tSuccessfully updated commands in guild "${guild.id}" for module "${module.id}" | new version = ${module.version}`
+      `\tSuccessfully updated commands in guild "${guild.id}" for module "${module.id}"`
     );
   }
 
