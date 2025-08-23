fd9349c32354f24580c01761014bff134ecbe1fb
RedsTom <tom.frefre.13@gmail.com>
2025-08-23 21:22:43 +0200

    chore: removed useless debug messages

--- src/modules/polls/commands/poll.command.ts
+++ src/modules/polls/commands/poll.command.ts
@@ -1,6 +1,5 @@
 import { MessageFlags, SlashCommandBuilder } from "discord.js";
 import { declareCommand } from "../../../lib/command.js";
-import logger from "../../../lib/logger.js";
 import pollCreationService from "../services/poll-creation.service.js";
 import { pollEditionMessage } from "../utils/poll.messages.js";
 
@@ -11,13 +10,7 @@ export default declareCommand({
   async execute(interaction) {
     const pollData = pollCreationService.getOrCreatePoll(interaction.user);
 
-    logger.debug(
-      `Poll data for user ${interaction.user.id} | data = ${pollData}`
-    );
-
-    logger.debug("Creating poll edition message");
     const editionMessage = pollEditionMessage(pollData);
-    logger.debug("Container builder created for poll edition");
 
     await interaction.reply({
       components: editionMessage,
