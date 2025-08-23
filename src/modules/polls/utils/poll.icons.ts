9b2da3f6af6b3556dc7605a66278d3b6898fca56
RedsTom <tom.frefre.13@gmail.com>
2025-08-23 21:22:57 +0200

    feat: add a send icon

--- src/modules/polls/utils/poll.icons.ts
+++ src/modules/polls/utils/poll.icons.ts
@@ -4,3 +4,4 @@ export const moveUpIcon = "<:moveup:1408084291044376586>";
 export const moveDownIcon = "<:movedown:1408084288410353814>";
 export const renameIcon = "<:rename:1408086699720052776>";
 export const okIcon = "<:ok:1408085609708257423>";
+export const sendIcon = "<:send:1408093518223904801>";
