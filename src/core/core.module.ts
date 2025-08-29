75c7589aaea91b118897d88038750c71a3ca9ee0
RedsTom <tom.frefre.13@gmail.com>
2025-08-29 19:40:13 +0200

    feat: add config type handler classes and module config editing interactions
    - Implement ConfigTypeHandler base class and handlers for string/number
      types
    - Add interaction handlers for configuring module options and toggling
      booleans
    - Register config type handlers and interactions in core module
    - Update config service with isConfigKey utility
    - Refine configuration message formatting and button logic

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,11 @@
 import { defineModule } from "../lib/module.js";
 import configCommand from "./commands/config.command.js";
 import moduleCommand from "./commands/module.command.js";
+import configTypeHandlers from "./config/config-type-handlers.js";
+import configureModuleButton from "./interactions/configure-module.button.js";
 import disableModuleButton from "./interactions/disable-module.button.js";
 import enableModuleButton from "./interactions/enable-module.button.js";
+import toggleOptionButton from "./interactions/toggle-option.button.js";
 import commandListener from "./listeners/interaction-create.listener.js";
 
 export default defineModule({
@@ -21,6 +24,12 @@ export default defineModule({
 
     registry.registerInteractionHandler(enableModuleButton);
     registry.registerInteractionHandler(disableModuleButton);
+    registry.registerInteractionHandler(toggleOptionButton);
+    registry.registerInteractionHandler(configureModuleButton);
+
+    for (const [, handler] of Object.entries(configTypeHandlers)) {
+      handler?.registerEditionInteractionHandlers(registry);
+    }
   },
   onInstall() {
     throw new Error("Core module cannot be installed or uninstalled.");
